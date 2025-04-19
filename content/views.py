da97ba8a8e8eaeba173b644ac29cb8341c2f4264
Jose Torres <jose.a.torres513@gmail.com>
2025-04-19 12:56:33 -0400

    FSDI_Experience Website

--- content/views.py
+++ content/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Project
+from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm
 
-# Create your views here.
+# List view
 def projects_list_view(request):
-    return render(request, "content/projects_list.html")
+    projects = Project.objects.all().order_by('-year')
+    return render(request, "content/projects_list.html", {"projects": projects})
+
+# Delete view
+@login_required
+def project_delete(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects_list")
+    return render(request, "content/project_delete_confirmation.html", {"project": project})
+
+# Create view
+def project_new_view(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save()
+            return redirect("projects_list")  # make sure this name matches your URL pattern
+    else:
+        form = ProjectForm()
+    return render(request, "content/project_new.html", {"form": form})
